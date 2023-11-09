8f17e33796e306101660438c21a62605965edf9b
Gurleen <gurleensinghdalli@gmail.com>
2023-11-09 09:34:29 -0400

    final changes done

--- javascript.js
+++ javascript.js
@@ -1,31 +1,25 @@
-// const ROCK = "rock";
-// const PAPER = "paper";
-// const SCISSOR = "scissor";
-// const options = [ROCK, PAPER, SCISSOR];
+const ROCK = "rock";
+const PAPER = "paper";
+const SCISSOR = "scissor";
+const options = [ROCK, PAPER, SCISSOR];
 
-// let playerCount = 0;
-// let computerCount = 0;
-// let gameInProgress = true;
+let playerCount = 0;
+let computerCount = 0;
+let gameInProgress = true;
 
-// function getComputerChoice(options) {
-//     const randomInt = Math.floor(Math.random() * 3);
-//     console.log(randomInt);
+function getComputerChoice(options) {
+    const randomInt = Math.floor(Math.random() * 3);
+    console.log(randomInt);
 
-//     return options[randomInt];
-// }
+    return options[randomInt];
+}
 
-// function getRoundResult(playerSelection, computerSelection) {
 
-// }
 
 
 
 function getRoundResult(playerSelection, computerSelection) {
-	/*
-      - I think in general switch statements are gross and ugly, but they have their time and place.
-      - Using switch(true) is a neat alternative to if/else:
-        https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/switch#an_alternative_to_if...else_chains
-    */
+	
 	switch (true) {
 		case computerSelection === playerSelection:
 			return "it's a draw";
@@ -40,4 +34,77 @@ function getRoundResult(playerSelection, computerSelection) {
 	}
 }
 
-console.log(getRoundResult("ROCK", "ROCK") );
+options.forEach(option => {
+    document.getElementById(option).addEventListener("click", event => {
+        if (!gameInProgress){
+            return;
+        }
+        playerSelection = option;
+        startGame(option, options);
+        event.target.classList.add("highlight");
+        checkForWinner();
+
+    });
+});
+
+document.addEventListener("mouseup", e => {
+    options.forEach(option => {
+        document.getElementById(option).classList.remove("highlight");
+    });
+});
+
+function startGame(playerSelection, options) {
+    const computerSelection = getComputerChoice(options);
+
+    const resultdisplay = document.getElementById("details");
+    resultdisplay.textContent = getRoundResult(playerSelection, computerSelection).toUpperCase();
+
+    const computerChoiceDisplay = document.querySelector(".btn-computer");
+    computerChoiceDisplay.textContent = computerSelection.toUpperCase();
+
+    const playerScoreDisplay = document.querySelector(".human");
+    playerScoreDisplay.textContent = playerCount;
+    const computerScoreDisplay = document.querySelector(".computer");
+	computerScoreDisplay.textContent = computerCount;
+};
+
+function checkForWinner() {
+    const playerWon = playerCount === 5;
+    const computerWon = computerCount === 6;
+
+
+    if (!playerWon && !computerWon) {
+    return;
+    }
+
+    const finalResult = document.getElementById("details");
+    finalResult.textContent = playerWon ? "You Won" : "You Lost";
+    addPlayAgainButton();
+    gameInProgress = false;
+}
+
+function addPlayAgainButton() {
+    const newGameButton = document.createElement("button");
+    newGameButton.textContent = "Play Again";
+    const detailsDisplay = document.getElementById("details");
+    newGameButton.style.backgroundColor = "silver";
+    detailsDisplay.appendChild(newGameButton);
+
+    newGameButton.addEventListener("click", () => {
+        resetGame();
+        detailsDisplay.textContent = "";
+        detailsDisplay.removeChild(newGameButton);
+    });
+
+}
+
+function resetGame() {
+    playerCount = 0;
+    computerCount = 0;
+    const playerScoreDisplay = document.querySelector(".human");
+	playerScoreDisplay.textContent = playerCount;
+	const computerScoreDisplay = document.querySelector(".computer");
+	computerScoreDisplay.textContent = computerCount;
+
+    gameInProgress = true;
+}
